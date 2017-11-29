bf618ab065e4e358ef330aa1be49b3c061d22d22
kurotsu7060 <hw16a072@oecu.jp>
2017-11-29 14:23:56 +0900

    no message

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -1,6 +1,7 @@
 #include "Game.hpp"
 
 
+<<<<<<< HEAD
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
 // TODO: 砲台を青い壁に沿って上下に動かす。(C) //kurotsu
@@ -9,6 +10,16 @@
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G) //kurotsu
 // TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
+=======
+// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)(実装:HW16A097 新甚 礁太)
+// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)(実装:HW16A209 谷津 峻哉)
+// TODO: 砲台を青い壁に沿って上下に動かす。(C)(実装:HW16A072 黒津 勇斗)
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)(実装:HW16A207 森本 義基)
+// TODO: スコアのサイズを大きくする。(E)(実装:HW16A097 新甚 礁太)
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)(実装:HW16A209 谷津 峻哉)
+// TODO: PlayBGM()関数を使って、BGMを再生する。(G)(実装:HW16A072 黒津 勇斗)
+// TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)(実装:HW16A207 森本 義基)
+>>>>>>> 9b816627f8d442cef882bd92fbdeb1242d2d521f
 
 
 Vector2 cloudPos;       //!< 雲の位置
@@ -48,7 +59,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
-            score += 1;         // スコアの加算
+            score += 100;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
@@ -86,7 +97,7 @@ void Update()
 
     // スコアの描画
     SetFont("nicoca_v1.ttf", 20.0f);
-    DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
-    DrawText(FormatString("%02d", score), Vector2(-320, 200), Color::white);
+    DrawText(FormatString("%05d", score), Vector2(-319, 199), Color::black);
+    DrawText(FormatString("%05d", score), Vector2(-320, 200), Color::white);
 }
 
